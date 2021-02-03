02f0c217da11e2f757a968a14712cb4fa72bc0f6
zvlasic <zlayo1@gmail.com>
2021-02-03 10:54:01 +0100

    Bump elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lasty.MixProject do
     [
       app: :lasty,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -46,7 +46,8 @@ defmodule Lasty.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:lastfmex, github: "zvlasic/lastfmex"}
     ]
   end
 
